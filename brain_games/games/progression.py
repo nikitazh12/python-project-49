11885d3b4ac2ec61dca0b3326fa05a767e0d5db7
nikitazh12 <nikitazh12@gmail.com>
2025-03-16 14:29:35 +0300

    fixed progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,21 +1,19 @@
 import random
 
-
 def generate_question_and_answer():
-    start = random.randint(1, 50)
-    step = random.randint(1, 10)
-    length = random.randint(5, 15)
-    hidden_index = random.choice(0, length - 1)
-    
+    start = random.randint(1, 50)  
+    step = random.randint(1, 10)   
+    length = random.randint(5, 15) 
+    hidden_index = random.randint(0, length - 1)
+
     progression = [str(start + step * i) for i in range(length)]
-    
+
     correct_answer = progression[hidden_index]
-    
+
     progression[hidden_index] = '..'
     question = ' '.join(progression)
-    
-    return question, correct_answer
 
+    return question, correct_answer
 
 def get_game_description():
     return 'What number is missing in the progression?'
